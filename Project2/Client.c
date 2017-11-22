70d49923ab12e1a408e6ff2ee2028e0aa9f687cd
schreck <schreck@iastate.edu>
2017-11-22 14:21:43 -0600

    Added the ability to make all of the files at once

--- Project2/Client.c
+++ Project2/Client.c
@@ -16,13 +16,13 @@
 /*						PROTOTYPES					*/
 /****************************************************/
 void PrintHash(unsigned char *hash){
-  if(hash == NULL){
-    printf("HASH NULL\n");
-    return;
-  }
-  for (int i = 0; i < SHA_DIGEST_LENGTH; i++){
-    printf("%02X", hash[i]);
-  }
+	if(hash == NULL){
+		printf("HASH NULL\n");
+		return;
+	}
+	for (int i = 0; i < SHA_DIGEST_LENGTH; i++){
+		printf("%02X", hash[i]);
+	}
 	printf("\n");
 }
 
@@ -31,61 +31,61 @@ void PrintHash(unsigned char *hash){
 /****************************************************/
 int main(){
 
-//STEPS:
-//1 -- Get user input (scan single line from the terminal)
+	//STEPS:
+	//1 -- Get user input (scan single line from the terminal)
 
-//2 -- Hash user input using SHA1
-while(1){
-	char s[256];
-	printf("enter the string : ");
-	fgets(s, 256, stdin);
-	char signature[256] = "";
+	//2 -- Hash user input using SHA1
+	while(1){
+		char s[256];
+		printf("enter the string : ");
+		fgets(s, 256, stdin);
+		char signature[256] = "";
 
-	unsigned char hash[SHA_DIGEST_LENGTH];
-	size_t length = strlen(s);
-	//printf("length %d\n", length);
-	SHA1(s, length,hash);
+		unsigned char hash[SHA_DIGEST_LENGTH];
+		size_t length = strlen(s);
+		//printf("length %d\n", length);
+		SHA1(s, length,hash);
 
-	//For debugging -- fixed issue of mismatching hashes for same string input
-	PrintHash(hash);//<-- message digest
+		//For debugging -- fixed issue of mismatching hashes for same string input
+		PrintHash(hash);//<-- message digest
 
-	//stringToEncodedAscii(hash);//<-- message signature
-	strcat(signature,stringToEncodedAscii(hash));
-	printf("\n");
-	//printf("signature :: %s\n",signature);
+		//stringToEncodedAscii(hash);//<-- message signature
+		strcat(signature,stringToEncodedAscii(hash));
+		printf("\n");
+		//printf("signature :: %s\n",signature);
 
-  int clientSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
-  int valread;
+		int clientSocket;
+		char buffer[1024];
+		struct sockaddr_in serverAddr;
+		socklen_t addr_size;
+		int valread;
 
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(8000);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+		clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-  
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_port = htons(8000);
+		serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-	//SEND THE STRING FIRST
-	send(clientSocket, s, strlen(s),0);
 
-  valread = read(clientSocket, buffer, 1024);
-  if(valread < 0){
-	printf("no response\n");
-  }else{
-	printf("%s\n",buffer);
-	send(clientSocket,signature,strlen(signature),0);
-  }
-	//read the response
-	char matchBuf[256];
-	valread = read(clientSocket, matchBuf, 256);
-	printf("%s\n",matchBuf);
-}
-  return 0;
+		addr_size = sizeof serverAddr;
+		connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+
+		//SEND THE STRING FIRST
+		send(clientSocket, s, strlen(s),0);
+
+		valread = read(clientSocket, buffer, 1024);
+		if(valread < 0){
+			printf("no response\n");
+		}else{
+			printf("%s\n",buffer);
+			send(clientSocket,signature,strlen(signature),0);
+		}
+		//read the response
+		char matchBuf[256];
+		valread = read(clientSocket, matchBuf, 256);
+		printf("%s\n",matchBuf);
+	}
+	return 0;
 }
--- Project2/Server.c
+++ Project2/Server.c
@@ -19,71 +19,71 @@
 /*						MAIN					*/
 /************************************************/
 int main(){
-  int welcomeSocket, newSocket;
-  int valread;
-  char buffer[1024];
+	int welcomeSocket, newSocket;
+	int valread;
+	char buffer[1024];
 	char hashBuffer[256];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size;
 
 
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(8000);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+	welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(8000);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-  if(listen(welcomeSocket,5)==0){
-    printf("Listening\n");
-  }else{
-    printf("Error\n");
-  }
+	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-while(1){
-  addr_size = sizeof serverStorage;
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-  pid_t pid = fork();
-  if(pid==0){
-
-	valread = read(newSocket, buffer, 1024);
-	if(valread<0){
-		printf("error no string send\n");
+	if(listen(welcomeSocket,5)==0){
+		printf("Listening\n");
 	}else{
-		printf("%s\n",buffer);
-
-		char *test = "string received\n";
-		send(newSocket, test, strlen(test), 0);
-	
-		//read in the signature
-		valread = read(newSocket, hashBuffer, 1024);
-		printf("encoded string from client:: %s\n",hashBuffer);
-	
-		printf("\n");
-
-		unsigned char encode[SHA_DIGEST_LENGTH];
-		size_t len = strlen(buffer);
-		SHA1(buffer,len,encode);
-
-		char serverSign[256]="";
-		strcat(serverSign, stringToEncodedAscii(encode));
-		printf("encoded string upon server:: %s\n",serverSign);
-
-		int cmp = strcmp(serverSign, hashBuffer);
-		if(cmp == 0){
-			char *result = "true";
-			send(newSocket, result, strlen(result),0);
-		}else{
-
-			char *result = "false";
-			send(newSocket, result, strlen(result),0);
-		}
+		printf("Error\n");
 	}
-  }
-	sleep(1);
-}
-  return 0;
+
+	while(1){
+		addr_size = sizeof serverStorage;
+		newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+		pid_t pid = fork();
+		if(pid==0){
+
+			valread = read(newSocket, buffer, 1024);
+			if(valread<0){
+				printf("error no string send\n");
+			}else{
+				printf("%s\n",buffer);
+
+				char *test = "server received msg\n";
+				send(newSocket, test, strlen(test), 0);
+
+				//read in the signature
+				valread = read(newSocket, hashBuffer, 1024);
+				printf("encoded string from client:: %s\n",hashBuffer);
+
+				printf("\n");
+
+				unsigned char encode[SHA_DIGEST_LENGTH];
+				size_t len = strlen(buffer);
+				SHA1(buffer,len,encode);
+
+				char serverSign[256]="";
+				strcat(serverSign, stringToEncodedAscii(encode));
+				printf("encoded string upon server:: %s\n",serverSign);
+
+				int cmp = strcmp(serverSign, hashBuffer);
+				if(cmp == 0){
+					char *result = "true";
+					send(newSocket, result, strlen(result),0);
+				}else{
+
+					char *result = "false";
+					send(newSocket, result, strlen(result),0);
+				}
+			}
+		}
+		sleep(1);
+	}
+	return 0;
 }
